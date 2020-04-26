4619cbf596c03dd5666b13134bac75dd322d8b86
yagince <straitwalk@gmail.com>
2020-04-27 01:08:39 +0900

    unselect with empty items

--- src/util/stateful_list.rs
+++ src/util/stateful_list.rs
@@ -23,35 +23,31 @@ impl<T> StatefulList<T> {
 
     pub fn set_items(&mut self, items: Vec<T>) {
         self.items = items;
-        self.state.select(Some(0));
+        if self.items.len() > 0 {
+            self.state.select(Some(0));
+        } else {
+            self.unselect();
+        }
     }
 
     pub fn next(&mut self) {
-        let i = match self.state.selected() {
+        match self.state.selected() {
             Some(i) => {
-                if i >= self.items.len() - 1 {
-                    0
-                } else {
-                    i + 1
-                }
+                let i = if i >= self.items.len() - 1 { 0 } else { i + 1 };
+                self.state.select(Some(i));
             }
-            None => 0,
-        };
-        self.state.select(Some(i));
+            None => self.unselect(),
+        }
     }
 
     pub fn previous(&mut self) {
-        let i = match self.state.selected() {
+        match self.state.selected() {
             Some(i) => {
-                if i == 0 {
-                    self.items.len() - 1
-                } else {
-                    i - 1
-                }
+                let i = if i == 0 { self.items.len() - 1 } else { i - 1 };
+                self.state.select(Some(i));
             }
-            None => 0,
-        };
-        self.state.select(Some(i));
+            None => self.unselect(),
+        }
     }
 
     pub fn unselect(&mut self) {
