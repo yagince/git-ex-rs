1abf8c210458b3427b33f3a9238d01d518df85f2
yagince <straitwalk@gmail.com>
2020-04-26 21:47:24 +0900

    layout base

---
+++ src/main.rs
@@ -0,0 +1,170 @@
+use std::{
+    error::Error,
+    io::{self, Write},
+};
+use termion::{
+    cursor::Goto, event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen,
+};
+use tui::{
+    backend::TermionBackend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
+    widgets::{Block, Borders, List, Paragraph, Text},
+    Terminal,
+};
+
+use gitex::util::{
+    event::{Event, Events},
+    StatefulList,
+};
+
+use unicode_width::UnicodeWidthStr;
+
+enum InputMode {
+    Normal,
+    Editing,
+}
+
+/// App holds the state of the application
+struct App {
+    /// Current value of the input box
+    input: String,
+    /// Current input mode
+    input_mode: InputMode,
+    /// History of recorded messages
+    messages: Vec<String>,
+}
+
+impl Default for App {
+    fn default() -> App {
+        App {
+            input: String::new(),
+            input_mode: InputMode::Editing,
+            messages: Vec::new(),
+        }
+    }
+}
+
+const TOP_MARGIN: u16 = 1;
+const HELP_MESSAGE_HEIGHT: u16 = 1;
+const TEXT_INPUT_HEIGHT: u16 = 3;
+const LIST_WIDTH_PERCENTAGE: u16 = 40;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Terminal initialization
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // Setup event handlers
+    let mut events = Events::new();
+
+    // Create default app state
+    let mut app = App::default();
+
+    loop {
+        // Draw UI
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(TOP_MARGIN)
+                .constraints(
+                    [
+                        Constraint::Length(HELP_MESSAGE_HEIGHT),
+                        Constraint::Length(TEXT_INPUT_HEIGHT),
+                        Constraint::Min(1),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+
+            let msg = match app.input_mode {
+                InputMode::Normal => "Press q to exit, e to start editing.",
+                InputMode::Editing => "Press Esc or Ctrl+c to exit, Enter to record the message",
+            };
+
+            // help message
+            let text = [Text::raw(msg)];
+            let help_message = Paragraph::new(text.iter());
+            f.render_widget(help_message, chunks[0]);
+
+            // input
+            let text = [Text::raw(&app.input)];
+            let input = Paragraph::new(text.iter())
+                .style(Style::default().fg(Color::Yellow))
+                .block(Block::default().borders(Borders::ALL).title("Input"));
+            f.render_widget(input, chunks[1]);
+
+            {
+                // main area
+                let chunks = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints(
+                        [
+                            Constraint::Percentage(LIST_WIDTH_PERCENTAGE),
+                            Constraint::Percentage(100 - LIST_WIDTH_PERCENTAGE),
+                        ]
+                        .as_ref(),
+                    )
+                    .split(chunks[2]);
+                // messages
+                let messages = app
+                    .messages
+                    .iter()
+                    .enumerate()
+                    .map(|(i, m)| Text::raw(format!("{}: {}", i, m)));
+                let messages = List::new(messages)
+                    .block(Block::default().borders(Borders::ALL).title("Messages"));
+                f.render_widget(messages, chunks[0]);
+            }
+        })?;
+
+        // Put the cursor back inside the input box
+        write!(
+            terminal.backend_mut(),
+            "{}",
+            Goto(
+                TOP_MARGIN + 2 + app.input.width() as u16,
+                TOP_MARGIN + HELP_MESSAGE_HEIGHT + TEXT_INPUT_HEIGHT - 1
+            )
+        )?;
+
+        // stdout is buffered, flush it to see the effect immediately when hitting backspace
+        io::stdout().flush().ok();
+
+        // Handle input
+        match events.next()? {
+            Event::Input(input) => match app.input_mode {
+                InputMode::Normal => match input {
+                    Key::Char('e') => {
+                        app.input_mode = InputMode::Editing;
+                        events.disable_exit_key();
+                    }
+                    Key::Char('q') => {
+                        break;
+                    }
+                    _ => {}
+                },
+                InputMode::Editing => match input {
+                    Key::Char('\n') => {
+                        app.messages.push(app.input.drain(..).collect());
+                    }
+                    Key::Char(c) => {
+                        app.input.push(c);
+                    }
+                    Key::Backspace => {
+                        app.input.pop();
+                    }
+                    Key::Esc | Key::Ctrl('c') => {
+                        break;
+                    }
+                    _ => {}
+                },
+            },
+            _ => {}
+        }
+    }
+    Ok(())
+}
---
+++ src/util.rs
@@ -0,0 +1,5 @@
+pub mod event;
+pub mod stateful_list;
+
+pub use event::{Event, Events};
+pub use stateful_list::StatefulList;
